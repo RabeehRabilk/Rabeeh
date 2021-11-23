f4fadf523125a4d298e5e00f14258e25d4976f8d
RabeehRabilk <94837093+RabeehRabilk@users.noreply.github.com>
2021-11-23 19:15:55 +0530

    Update name.sh

--- name.sh
+++ name.sh
@@ -1,3 +1,4 @@
+chmod +x *
 clear
 figlet "Rabeeh"
 read -p  "[+]Eenter your name :" NAME
@@ -6,6 +7,6 @@ clear
 figlet "Rabeeh"
 read -p  "[+]Eenter your age : " AGE
 clear
-echo  "your name is "
+echo  "my name is "
 figlet   "$NAME"
-echo "you age is $age"
+echo "my age is $AGE"
