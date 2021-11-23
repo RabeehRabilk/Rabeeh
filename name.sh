1222dd06b7b354e997f3faf511394adda71a1e0f
RabeehRabilk <94837093+RabeehRabilk@users.noreply.github.com>
2021-11-23 16:13:31 +0530

    name. Sh
    bash name.sh
    ./name.sh

---
+++ name.sh
@@ -0,0 +1,11 @@
+clear
+figlet "Rabeeh"
+read -p  "[+]Eenter your name :" NAME
+
+clear
+figlet "Rabeeh"
+read -p  "[+]Eenter your age : " AGE
+clear
+echo  "your name is "
+figlet   "$NAME"
+echo "you age is $age"
